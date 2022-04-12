5c084dd3e37129910a0f943291e3d2e754ac67de
JeongEunChang <101631103+JeongEunChang@users.noreply.github.com>
2022-04-12 19:51:02 +0900

    22/04/12 수업
    객체 개념, 입출력 함수

---
+++ Client_CPP/DynamicAllocation/DynamicAllocation.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <stdlib.h>
+
+struct Mystruct
+{
+    int a;
+};
+
+int main()
+{
+    // c 에서 동적할당
+    struct Mystruct* a = (Mystruct*)malloc(sizeof(Mystruct));
+    free(a);
+
+    // c++ 에서 동적할당
+    // new 키워드를 사용
+    // 타입* 포인터 이름 = new 타입;
+    Mystruct* mystruct = new Mystruct;
+    delete mystruct;
+
+    
+    int* pi = new int;
+    *pi = 100;
+    std::cout << *pi << std::endl;
+    delete pi;
+
+    
+    return 0;
+}
